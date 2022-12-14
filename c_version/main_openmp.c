25b503d0088d002ba3326884a8e07cb971f57bc3
Michael Carlstrom <rmc170@case.edu>
2022-12-13 16:07:54 -0800

    closer

--- c_version/main_openmp.c
+++ c_version/main_openmp.c
@@ -10,27 +10,38 @@ typedef void (*func)(int *, int, int, int);
 
 // Utility functions
 
-unsigned int rand_interval(unsigned int min, unsigned int max)
+// unsigned int rand_interval(unsigned int min, unsigned int max)
+// {
+//    // https://stackoverflow.com/questions/2509679/
+//    int r;
+//    const unsigned int range = 1 + max - min;
+//    const unsigned int buckets = RAND_MAX / range;
+//    const unsigned int limit = buckets * range;
+
+//    do
+//    {
+//       r = rand();
+//    } while (r >= limit);
+
+//    return min + (r / buckets);
+// }
+
+void swap(int *arr, int i, int j)
 {
-   // https://stackoverflow.com/questions/2509679/
-   int r;
-   const unsigned int range = 1 + max - min;
-   const unsigned int buckets = RAND_MAX / range;
-   const unsigned int limit = buckets * range;
-
-   do
-   {
-      r = rand();
-   } while (r >= limit);
-
-   return min + (r / buckets);
+   int temp = arr[i];
+   arr[i] = arr[j];
+   arr[j] = temp;
 }
 
 void fillupRandomly(int *m, int size, unsigned int min, unsigned int max)
 {
-   int i;
-   for (i = 0; i < size; i++)
-      m[i] = rand_interval(min, max);
+   m[0] = 0;
+   for (int i = 1; i < size; i++)
+   {
+      m[i] = i;
+
+      swap(m, i, rand() % i);
+   }
 }
 
 void printArray(int *a, int size)
@@ -48,13 +59,6 @@ int isSorted(int *a, int size)
       if (a[i] > a[i + 1])
          return 0;
    return 1;
-}
-
-void swap(int *arr, int i, int j)
-{
-   int temp = arr[i];
-   arr[i] = arr[j];
-   arr[j] = temp;
 }
 
 void compAndSwap(int *a, int i, int j, int dir)
@@ -174,15 +178,94 @@ int cmpfunc(const void *a, const void *b)
 {
    int va = *(const int *)a;
    int vb = *(const int *)b;
+   // print("va =%i ")
    return (va > vb) - (va < vb);
+}
+
+int sameElements(int *arr1, int *arr2, int l)
+{
+   for (int i = 0; i < l; i++)
+   {
+      if (arr1[i] != arr2[i])
+      {
+         return 0;
+      }
+   }
+   return 1;
+}
+
+void uniqueElement(int *arr, int length)
+{
+   for (int i = 0; i < length; i++)
+   {
+      for (int j = 0; j < length; j++)
+      {
+         if (arr[j] == arr[i] && i != j)
+         {
+            assert(0);
+         }
+      }
+   }
+}
+
+int getNumAmount(int *arr, int length, int val)
+{
+   int count = 0;
+   for (int i = 0; i < length; i++)
+   {
+      if (val == arr[i])
+      {
+         count++;
+      }
+   }
+   return count;
+}
+
+int allValsSame(int *arr, int length)
+{
+   int val = arr[0];
+   for (int i = 1; i < length; i++)
+   {
+      if (val != arr[i])
+      {
+         return 0;
+      }
+   }
+   return 1;
+}
+
+int getBucketIndex(int e, int *splitter, int splitterCount, int p)
+{
+   if (e <= splitter[0])
+   {
+      return 0;
+   }
+   else if (e >= splitter[splitterCount - 1])
+   {
+      return p - 1;
+   }
+   else
+   {
+      for (int l = 1; l < splitterCount; l++)
+      {
+         if (splitter[l - 1] < e && e <= splitter[l])
+         {
+            return l;
+         }
+      }
+   }
+   assert(0);
 }
 
 int *sampleSortHelper(int arr[], int p, int k, int length)
 {
-   printArray(arr, length);
-   // SPECIAL CASE
+   //Testing
+   int *copy = (int *)malloc(length * sizeof(int));
+   memcpy(copy, arr, length * sizeof(int));
+   //  SPECIAL CASE
    if (p == 1)
    {
+      printf("Special Case of One Bucket just calls qsort");
       qsort(arr, length, sizeof(arr[0]), cmpfunc);
       return arr;
    }
@@ -192,6 +275,7 @@ int *sampleSortHelper(int arr[], int p, int k, int length)
       qsort(arr, length, sizeof(arr[0]), cmpfunc);
       return arr;
    }
+   
    // More Threads than min bucket Size
    if (p > k)
    {
@@ -201,119 +285,69 @@ int *sampleSortHelper(int arr[], int p, int k, int length)
    // Generates Splitters
    int splitterCount = p - 1;
    int splitter[splitterCount];
-   for (int i = 0; i < splitterCount; i++)
-   {
-      // TODO check if same rand num breaks
-      int r = rand() % length;
-      splitter[i] = arr[r];
-      // printf("r= %i\n", r);
-      // printf("rand num %i\n", arr[r]);
-      // printf("rand num %i\n", splitter[i]);
+   for (int i = 0; i < splitterCount - 1;i++){
+      splitter[i] = arr[rand() % length];
    }
-   // printArray(splitter, splitterCount);
-   // printf("\n");
-   qsort(splitter, splitterCount, sizeof(splitter[0]), cmpfunc);
-   // printArray(splitter, splitterCount);
-   // printf("\n");
-   // printArray(splitter, p);
+      qsort(splitter, splitterCount, sizeof(splitter[0]), cmpfunc);
 
    // Not effiecent memory wise
-   int buckets[p][length];
+   // Generate Buckets
+   int *buckets[p];
    int indicies[p];
    for (int i = 0; i < p; i++)
    {
+      buckets[i] = (int *)malloc(length * sizeof(int));
       indicies[i] = 0;
    }
 
-   // TODO parallel
-   printf("Thread Count =  %i\n", p);
-   // printArray(arr, length);
-   // printArray(splitter, splitterCount);
-   // printf("\n");
+#pragma omp parallel
+{
+   #pragma omp for
+   //Divide  
    for (int j = 0; j < length; j++)
    {
-
-      // printArray(indicies, p);
-      // printf("\n");
-      // printf("pain %i", splitter[splitterCount]);
-      if (arr[j] <= splitter[0])
+      #pragma omp crital
       {
-         // printf("bucket 0 %i\n", arr[j]);
-         buckets[0][indicies[0]] = arr[j];
-         indicies[0] = indicies[0] + 1;
-      }
-      else if (arr[j] >= splitter[splitterCount-1])
-      {
-         // printf("bucket p %i\n", arr[j]);
-         buckets[p-1][indicies[p-1]] = arr[j];
-         indicies[p-1] = indicies[p-1] + 1;
-      }
-      else
-      {
-         for (int l = 1; l < splitterCount; l++)
-         {
-            if (splitter[l - 1] < arr[j] && arr[j] <= splitter[l])
-            {
-               // printf("bucket l %i %i\n", l, arr[j]);
-               buckets[l][indicies[l]] = arr[j];
-               indicies[l] = indicies[l] + 1;
-               continue;
-            }
-         }
+      int bucketNum = getBucketIndex(arr[j], splitter, splitterCount, p);
+      buckets[bucketNum][indicies[bucketNum]] = arr[j];
+      indicies[bucketNum] = indicies[bucketNum] + 1;
       }
    }
-   // printArray(arr, length);
-   // printf("\n");
 
-   // printArray(splitter, splitterCount);
-   // printf("\n");
-   // // printArray(indicies, p);
-   // // printf("\n");
-
-   // for (int i = 0; i < p; i++)
-   // {
-   //    printArray(buckets[i], length);
-   //    printf("\n");
-   // }
-
-   int solved[length];
+#pragma omp for
+//Conquer
    for (int i = 0; i < p; i++)
    {
-      // buckets[i] = realloc(buckets[i], indicies[i] * sizeof(buckets[0][0]));
       int *bucket = sampleSortHelper(buckets[i], p, k, indicies[i]);
-      // printArray(bucket, indicies[i]);
-      // printf("%i bucket length", indicies[i]);
-      printArray(indicies, p);
       int startingPoint = 0;
-
-      // if (i > 0)
-      // {
-         for (int l = 0; l < i; l++)
-         {
-            startingPoint = startingPoint + indicies[l];
-            // printf("starting point %i indicies[i-1] %i i=%i\n", startingPoint,indicies[l],l);
-         }
-      // }
-      // printf("bucket size%li", sizeof(bucket));
-      // printf("starting point %i\n", startingPoint);
-      memcpy(solved + startingPoint, bucket, indicies[i] * sizeof(int));
-      printArray(solved, length);
+      for (int l = 0; l < i; l++)
+      {
+         startingPoint = startingPoint + indicies[l];
+      }
+      memcpy(arr + startingPoint, bucket, indicies[i] * sizeof(int));
+      free(buckets[i]);
    }
-   printArray(solved, length);
-   memcpy(arr, solved, length);
-
-   return arr;
-
-   // printf("%i ", r);
-
-   // printArray(arr, length);
 }
 
-void sampleSort(int array[], int l, int h, int dir)
+   //TESTING
+   qsort(copy, length, sizeof(int), cmpfunc);
+   if (!sameElements(arr, copy, length))
+   {
+      printArray(arr, length);
+      printArray(copy, length);
+   }
+   assert(sameElements(arr, copy, length));
+   free(copy);
+
+
+   return arr;
+}
+
+void sampleSort(int *arr, int l, int h, int dir)
 {
-   int k = 10;
+   int k = 1024;
    int p = omp_get_num_threads();
-   array = sampleSortHelper(array, p, k, h);
+   memcpy(arr, sampleSortHelper(arr, p, k, h), h * sizeof(int));
 }
 
 int runExperiments(int up, int low, int high, int print)
@@ -325,18 +359,26 @@ int runExperiments(int up, int low, int high, int print)
 
    // Experiment value setup
    // 67108864, 16777216, 2097152
-   int arraySizes[] = {10};
+   int arraySizes[] = {10886004};
    int threadCount[] = {1, 4};
    int i;
    for (i = 0; i < sizeof(arraySizes) / sizeof(arraySizes[0]); i++)
    {
       int N = arraySizes[i];
       int *X = (int *)malloc(N * sizeof(int));
-      // Dealing with fail memory allocation
+      int *Y = (int *)malloc(N * sizeof(int));
+
+      // Dealing with faisolvedl memory allocation
       if (!X)
       {
          if (X)
             free(X);
+         return (EXIT_FAILURE);
+      }
+      if (!Y)
+      {
+         if (Y)
+            free(Y);
          return (EXIT_FAILURE);
       }
 
@@ -356,7 +398,7 @@ int runExperiments(int up, int low, int high, int print)
 
             // Have to fill up with random numbers every time since sorts in place
             fillupRandomly(X, N, low, high);
-
+            memcpy(Y, X, N * sizeof(int));
             if (print == 1)
             {
                printArray(X, N);
@@ -378,11 +420,21 @@ int runExperiments(int up, int low, int high, int print)
             // CSV
             fprintf(fpt, "%s, %s, %i, %i, %f\n", sortingNames[j], implemenation, threadCount[k], N, end - begin);
 
-            // assert(1 == isSorted(X, N));
+            assert(isSorted(X, N));
+
+            qsort(Y, N, sizeof(int), cmpfunc);
+            if (!sameElements(X, Y, N))
+            {
+               printArray(X, 10);
+               printArray(Y, 10);
+            }
+            assert(sameElements(X, Y, N));
+
             printf("Sorted\n");
          }
       }
       free(X);
+      free(Y);
       fclose(fpt);
    }
    return 0;
