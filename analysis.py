fixed trials/averaging data

analysis.py
@@ -17,20 +17,23 @@ def customPlot(data: DataFrame, across: str):
 
     if threadCount == across:
         data = data[data[elements] == max(data[elements])]
-        X =data[[algorithm,threadCount,time]]
+        X = data[[algorithm,threadCount,time]]
     else:
         data = data[data[threadCount] == max(data[threadCount])]
         X =data[[algorithm,elements,time]]
   
+    
     customcmap = ["red", "blue", "green","grey","orange","purple"]
+    
     
     fig, axis = plot.subplots(figsize=(8, 6))
     algorithmNames = np.unique(X[algorithm])
 
     for i in range(len(algorithmNames)):
-        algoData = X[X[algorithm] == algorithmNames[i]]
+        algoData = X[X[algorithm] == algorithmNames[i]].groupby(across).mean(numeric_only=True).reset_index()
         x = np.array(algoData[across])
         y = np.array(algoData[time])
+
         plot.plot(x, y,label=str(algorithmNames[i]), marker='o', color=customcmap[i])
 
     plot.legend(loc='best')